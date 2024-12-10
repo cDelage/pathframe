b58f57f80d0e0c82a6af3d0d460353c40ece8573
cDelage <42372508+cDelage@users.noreply.github.com>
2024-12-10 16:52:19 +0100

    Cli first version

---
+++ pathframe_cli/src/args.rs
@@ -0,0 +1,29 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "pframe", about = "Pathframe CLI tool")]
+pub struct PathframeArgs {
+    #[command(subcommand)]
+    pub command: EntityCommands,
+    #[arg(short = 'w', long = "workspace")]
+    pub workspace: Option<String>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum EntityCommands {
+    ///Manage the application prototypes (list, read...)
+    #[command(aliases=["app","app-proto"])]
+    ApplicationPrototype(ApplicationPrototypeCommands),
+}
+
+#[derive(Debug, Args)]
+pub struct ApplicationPrototypeCommands {
+    #[clap(subcommand)]
+    pub command: ApplicationPrototypeSubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ApplicationPrototypeSubCommands {
+    ///Find all application prototypes into workspace
+    List,
+}
--- pathframe_cli/src/main.rs
+++ pathframe_cli/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use args::{ApplicationPrototypeSubCommands, EntityCommands, PathframeArgs};
+use clap::Parser;
+
+use pathframe_lib::workspace::application_prototype::find_all_application_prototypes;
+use std::error::Error;
+
+mod args;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    let args = PathframeArgs::parse();
+
+    // Match sur les différentes commandes et arguments
+    let workspace = &args.workspace.ok_or("Fail to find workspace")?;
+    match args.command {
+        // Cas de la commande ApplicationPrototype
+        EntityCommands::ApplicationPrototype(subcommand) => {
+            match subcommand.command {
+                // Cas de la sous-commande List
+                ApplicationPrototypeSubCommands::List => {
+                    let application_prototypes = find_all_application_prototypes(workspace)?;
+                    
+                    application_prototypes.iter().for_each(|application| {
+                        println!("{}", application.application_name);
+                        println!("{}", application.description);
+                        println!("--------------------------------")
+                    });
+                    
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
